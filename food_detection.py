0848564541575dd1277e620860fd9fdd707dd5ce
genieu99 <yujin0419@sookmyung.ac.kr>
2023-09-10 14:41:43 +0900

    [Fix] 모델 크기 이슈로 커밋 되돌리기(#4)

--- food_detection.py
+++ food_detection.py
@@ -2,66 +2,64 @@ from PIL import Image
 import cv2
 import numpy as np
 import os
-
-dir_name = "crop"
-if not os.path.exists(dir_name):
-    os.mkdir(dir_name)
-
-# Yolo 로드
-net = cv2.dnn.readNet("yolov2-tiny.weights", "yolov2-tiny.cfg")
-classes = []
-with open("coco.names", "r") as f:
-    classes = [line.strip() for line in f.readlines()]
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-# 이미지 가져오기
-img = cv2.imread("datasets/food2.jpg")
-img = cv2.resize(img, None, fx=0.8, fy=0.8)
-height, width, channels = img.shape
-
-# Detecting objects
-blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-net.setInput(blob)
-outs = net.forward(output_layers)
-
-# 정보를 화면에 표시
-class_ids = []
-confidences = []
-boxes = []
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            # Object detected
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
-            # 좌표
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
-
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+from fastapi import UploadFile
 
 
-font = cv2.FONT_HERSHEY_PLAIN
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        # label = str(classes[class_ids[i]])
-        color = colors[i]
-        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        # cv2.putText(img, (x, y + 30), font, 3, color, 3)
-        
-        img_np = np.array(img)
-        img_bgr = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
-        image_to_crop = Image.fromarray(img_bgr)
-        food = image_to_crop.crop((x, y, x + w, y + h))
-        food.save(f'crop/image_{i+1}.png')
+def detect_and_crop_image(image: UploadFile, output_dir):
+    # Yolo 로드
+    net = cv2.dnn.readNet("yolov2-tiny.weights", "yolov2-tiny.cfg")
+
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    # 이미지 가져오기
+    content = image.file.read()
+    img = cv2.imdecode(np.fromstring(content, np.uint8), cv2.IMREAD_COLOR)
+    img = cv2.resize(img, None, fx=0.8, fy=0.8)
+    height, width, channels = img.shape
+
+    # 이미지 감지
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # 정보를 화면에 표시
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # 사진 탐색
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # 좌표 추출
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+
+            img_np = np.array(img)
+            img_bgr = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+            image_to_crop = Image.fromarray(img_bgr)
+            food = image_to_crop.crop((x, y, x + w, y + h))
+            food.save(os.path.join(output_dir, f'image_{i + 1}.png'))
