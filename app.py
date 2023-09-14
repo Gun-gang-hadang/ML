022b56cc93e51fbdc99e776b848ec212805795c3
genieu99 <yujin0419@sookmyung.ac.kr>
2023-09-14 17:19:59 +0900

    [Add] 필요한 버전 정리(#6)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import uvicorn
 import configparser
 import torch.utils.data
@@ -12,10 +13,7 @@ app = FastAPI(max_request_size = 1024*1024*1024)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model_path = tf.keras.models.load_model('./food_recognition_model2.h5')
 
-dir_name = "crop"
-if not os.path.exists(dir_name):
-    os.mkdir(dir_name)
-
+# 서버 주소 받아오기
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -30,7 +28,14 @@ async def root():
 
 @app.post("/api/v1/analyze/image")
 async def preprocess_image(image: UploadFile):
+    # 자른 이미지를 저장할 디렉토리 생성
+    dir_name = "crop"
     print("*")
+
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+    os.mkdir(dir_name)
+
     detect_and_crop_image(image, dir_name)
     food_name = predict_food()
     print(food_name)
